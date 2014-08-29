0b00459174bd4a33727461f08a29ce86216fcd8d
softprops <d.tangren@gmail.com>
2014-08-29 00:50:38 -0400

    a bit of codestyle

--- src/main/scala/Cache.scala
+++ src/main/scala/Cache.scala
@@ -20,14 +20,19 @@ object Cache {
     Lru(cap, initCap, ttl, ttidle)
 }
 
-case class Capped[K, V](capacity: Long, initCapacity: Int) extends Cache[K, V] {
+case class Capped[K, V](
+  capacity: Long,
+  initCapacity: Int
+) extends Cache[K, V] {
+
   private[this] val underlying =
     Collections.synchronizedMap(
       new LinkedHashMap[K, Future[V]](initCapacity))
 
   def get(k: K): Option[Future[V]] = Option(underlying.get(k))
 
-  def apply(k: K, make: () => Future[V])(implicit ec: ExecutionContext): Future[V] = {
+  def apply(k: K, make: () => Future[V])
+   (implicit ec: ExecutionContext): Future[V] = {
     val promise = Promise[V]()
     underlying.put(k, promise.future) match {
       case null =>
@@ -47,8 +52,12 @@ case class Capped[K, V](capacity: Long, initCapacity: Int) extends Cache[K, V] {
 }
 
 case class Lru[K, V](
-  capcity: Long, initCapacity: Int,
-  ttlive: FiniteDuration, ttidle: FiniteDuration) extends Cache[K, V]{
+  capcity: Long,
+  initCapacity: Int,
+  ttlive: FiniteDuration,
+  ttidle: FiniteDuration
+) extends Cache[K, V]{
+
   private[this] case class Entry[V](promise: Promise[V]) {
     val created = System.currentTimeMillis
     @volatile var touched = created
@@ -56,6 +65,7 @@ case class Lru[K, V](
     def touch() = touched = System.currentTimeMillis
     def live = true // todo base on config above
   }
+
   private[this] val underlying =
     Collections.synchronizedMap(
       new LinkedHashMap[K, Entry[V]](initCapacity))
